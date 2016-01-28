7ec168aeae73745e4e1230d2d9cdc260e040fd5d
guanyuxin <guanyuxin@meituan.com>
2016-01-28 22:38:10 +0800

    bugfix：房间回收太频繁

--- game/room.js
+++ game/room.js
@@ -4,10 +4,17 @@ var Game = require('./game.js');
 var roomID = 1;
 var rooms = [];
 
+//定期回收房间
 setInterval(function () {
 	for (var i = 0; i < rooms.length; i++) {
 		if (rooms[i].game.clients.length == 0 && !rooms[i].presist) {
-			rooms.splice(i, 1);
+			if (rooms.dead > 10) {
+				rooms.splice(i, 1);
+			} else if (rooms.dead > 0) {
+				rooms.dead++;
+			} else {
+				rooms.dead = 1;
+			}
 		}
 	}
 }, 1000);
