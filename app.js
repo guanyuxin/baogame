051d14fd2d7f40cbd6001ce7c7a00ac94a5dc4b5
guanyuxin <guanyuxin@meituan.com>
2018-06-01 22:28:01 +0800

    save static file

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ server.listen(opts.port || 8030, function () {
 });
 
 app.use('/static', express.static('static'));
+app.use('/invite', express.static('invite'));
 app.use('/build', express.static('build'));
 
 //app.use(cookieParser());
